847321c351e53653243fd9c0912052d6df9e90d0
hanaraya <n.harish22@gmail.com>
2016-03-13 12:44:00 -0700

    refactored - added src folder

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var eslint = require('gulp-eslint');
 gulp.task('run', function(){
 	var browserSync=require('browser-sync').create();
 	browserSync.init({
-		server: './'
+		server: 'src/'
 	});
 	browserSync.stream();
 });
@@ -23,30 +23,30 @@ gulp.task('run-dist', ['default'], function(){
 });
 
 gulp.task('compress-js', function() {
-	return gulp.src('js/*.js')
+	return gulp.src('src/js/*.js')
 	.pipe(uglify())
 	.pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('minify-css', function() {
-	return gulp.src('css/*.css')
+	return gulp.src('src/css/*.css')
 	.pipe(minifyCss({compatibility: 'ie8'}))
 	.pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('compress-html', function() {
-	return gulp.src('*.html')
+	return gulp.src('src/*.html')
 	.pipe(minifyInline())
 	.pipe(minifyHTML())
 	.pipe(gulp.dest('dist/'));
 });
 
 gulp.task('lint', function(){
-	return gulp.src('js/*.js').pipe(eslint()).pipe(eslint.format());
+	return gulp.src('src/js/*.js').pipe(eslint()).pipe(eslint.format());
 });
 
 gulp.task('copy-images', function(){
-	return gulp.src('images/*.png').pipe(gulp.dest('dist/images'));
+	return gulp.src('src/images/*.png').pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('default', ['compress-html', 'compress-js', 'minify-css', 'copy-images']);
